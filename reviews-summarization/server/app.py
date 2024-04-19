4cba7d77c0edfe1cc68cc1d0ebe90ec0824d5b9e
darIabizina <bizina_2004@mail.ru>
2024-04-19 14:33:16 +0300

    top_films

--- reviews-summarization/server/app.py
+++ reviews-summarization/server/app.py
@@ -1,27 +1,28 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for  # noqa # pylint: disable=unused-import
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+  return render_template("index.html")
 
 
 @app.route('/yes_clicked', methods=['POST'])
 def yes_clicked():
     # add to db
-    return render_template("index.html")
+  return render_template("index.html")
 
 
 @app.route('/no_clicked', methods=['POST'])
 def no_clicked():
     # add to db
-    return render_template("index.html")
+  return render_template("index.html")
 
 
 if __name__ == "__main__":
-    import logging
-    logger = logging.getLogger('waitress')
-    logger.setLevel(logging.DEBUG)
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8001)
+  import logging
+  logger = logging.getLogger('waitress')
+  logger.setLevel(logging.DEBUG)
+  from waitress import serve
+  serve(app, host="0.0.0.0", port=8001)
+
